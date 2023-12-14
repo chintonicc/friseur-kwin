510f08c929ff61948ab1f9249f677c7504bdc56f
domar <marceldo61@gmail.com>
2023-12-14 17:50:11 +0100

    deleted Appointment.css

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,39 +3,39 @@ import './Cards.css';
 import CardItem from './CardItem';
 
 function Cards() {
-  return (
-    <div className='cards'>
-      <h1 className='cards-text'>Was wir bieten:</h1>
-      <div className='cards__container'>
-        <div className='cards__wrapper'>
-          <ul className='cards__items'>
-            <CardItem
-              src='images/img-1.jpg'
-              text='Haarschnitt'
-            />
-            <CardItem
-              src='images/img-2.jpg'
-              text='Haarfärbung'
-            />
-          </ul>
-          <ul className='cards__items'>
-            <CardItem
-              src='images/img-3.jpg'
-              text='Dauerwelle'
-            />
-            <CardItem
-              src='images/img-4.jpg'
-              text='Haarpflege'
-            />
-            <CardItem
-              src='images/img-5.jpg'
-              text='Beratung'
-            />
-          </ul>
+    return (
+        <div className='cards'>
+            <h1 className='cards-text'>Was wir bieten:</h1>
+            <div className='cards__container'>
+                <div className='cards__wrapper'>
+                    <ul className='cards__items'>
+                        <CardItem
+                            src='images/img-1.jpg'
+                            text='Haarschnitt'
+                        />
+                        <CardItem
+                            src='images/img-2.jpg'
+                            text='Haarfärbung'
+                        />
+                    </ul>
+                    <ul className='cards__items'>
+                        <CardItem
+                            src='images/img-3.jpg'
+                            text='Dauerwelle'
+                        />
+                        <CardItem
+                            src='images/img-4.jpg'
+                            text='Haarpflege'
+                        />
+                        <CardItem
+                            src='images/img-5.jpg'
+                            text='Beratung'
+                        />
+                    </ul>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Cards;
